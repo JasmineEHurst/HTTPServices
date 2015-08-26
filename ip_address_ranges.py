533738e39d4fdfecb2892231e8d2c9ab33e4d08c
Jasmine Hurst <jasmine.ehurst@gmail.com>
2015-08-26 01:34:16 -0500

    Corrections to ip_address_ranges.py file

--- ip_address_ranges.py
+++ ip_address_ranges.py
@@ -1,6 +1,13 @@
 import ipaddress
+
 net = ipaddress.ip_network(u'123.45.67.64/27')
-net
-# IPv4Network(u'192.0.2.1/24')
+print(' IPv4 Network ranges:')
 for a in net:
-    print(net)
+    print(a)
+
+print('')
+
+net6 = ipaddress.ip_network(u'12:3456:78:90ab:cd:ef01:23:30/125')
+print('IPv6 Network ranges:')
+for a in net6:
+    print(a)
